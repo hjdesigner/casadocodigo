ae17b8f2c552e4fdc762833da2cae48ad44bc709
Henrique Melanda <hjdesigner23@gmail.com>
2020-04-08 22:14:58 -0300

    Autenticação de usuários e Autorização de acesso

--- src/app/modelos/livro.js
+++ src/app/modelos/livro.js
@@ -1,14 +1,12 @@
 const { check } = require('express-validator/check');
 
 class Livro {
-
-  static validacao() {
-    return [
-      check('titulo').isLength({ min: 5 }).withMessage('O título precisa ter no mínimo 5 caracteres!'),
-      check('preco').isCurrency().withMessage('O preço precisa ter um valor monetário válido!')
-    ]
-  }
-
+    static validacoes() {
+        return [
+            check('titulo').isLength({ min: 5 }).withMessage('O título precisa ter no mínimo 5 caracteres!'),
+            check('preco').isCurrency().withMessage('O preço precisa ter um valor monetário válido!')
+        ];
+    }
 }
 
 module.exports = Livro;
